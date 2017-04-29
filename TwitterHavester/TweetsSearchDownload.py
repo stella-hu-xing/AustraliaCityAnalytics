19ca0028b531964b53e9238641a3b8bab1206111
Xing Hu <xinghu0164@gmail.com>
2017-04-29 13:51:55 +1000

    combine into one db and check duplication

--- TwitterHavester/TweetsSearchDownload.py
+++ TwitterHavester/TweetsSearchDownload.py
@@ -1,17 +1,17 @@
-#Import the necessary methods from tweepy library
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+# Import the necessary methods from tweepy library
+
 import tweepy
 import time
 import json
 from couchdb import Server
-import couchdb
 
-remote_server = Server()
-db = remote_server['search_tweets']
+server = Server()
+try:
+    db = server['tweets']
+except:
+    db = server.create('tweets')
 
-#Var  iables that contains the user credentials to access Twitter API 
+# Var  iables that contains the user credentials to access Twitter API
 access_token = "546643485-WaT1mm4EwJe2RrnxMk5xfNxiUxnvfHc3HQgk6jFO"
 access_token_secret = "GbaChVqU98h8NpUQ1FzfSG2AonWFBVdtalv5d9LNDfUrU"
 consumer_key = "Orgmlwvc3OVi8UtyB1Idk1ArM"
@@ -22,51 +22,53 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-#Put your search term
+# Put your search term
 searchquery = "Donald Trump"
 
-contents =tweepy.Cursor(api.search,q=searchquery,lang="en").items()
+contents = tweepy.Cursor(api.search, q=searchquery, lang="en").items()
 count = 0
-errorCount=0
+errorCount = 0
 
-
-#file = open('tweets_search_data.json', 'wb') 
 
 while True:
     try:
         content = next(contents)
         count += 1
-        #use count-break during dev to avoid twitter restrictions
-        #if (count>10):
+        # use count-break during dev to avoid twitter restrictions
+        # if (count>10):
         #    break
     except tweepy.TweepError:
-        #catches TweepError when rate limiting occurs, sleeps, then restarts.
-        #nominally 15 minnutes, make a bit longer to avoid attention.
+        # catches TweepError when rate limiting occurs, sleeps, then restarts.
+        # nominally 15 minnutes, make a bit longer to avoid attention.
         print "sleeping...."
-        time.sleep(60*16)
+        time.sleep(60 * 16)
         content = next(contents)
     except StopIteration:
         break
     try:
-        print "Writing to JSON tweet number:"+str(count)
-       # json.dump(content._json,file,sort_keys = True,indent = 4)
+        print "Writing to JSON tweet number:" + str(count)
+        # json.dump(content._json,file,sort_keys = True,indent = 4)
 
-        njson = json.dumps(content._json,ensure_ascii=False)
+        njson = json.dumps(content._json, ensure_ascii=False)
         doc = json.loads(njson)
         nid = doc['id_str']
-     
-        ntext = doc['text']
-  
-        ncoordinates = doc['coordinates']
-        nuser = doc['user']
-        ntime = doc['created_at']
-        nplace = doc['place']
-       # nid = content._json.u'id'
-        ndoc={'_id':nid,'text':ntext, 'user':nuser, 'coordinates':ncoordinates, 'create_time':ntime,'place':nplace,'addressed':False}
-        db.save(ndoc)
-        print('-------------------------------------')
+
+        if nid in db:
+            print('--------aleardy have----------------')
+
+        else:
+            ntext = doc['text']
+            ncoordinates = doc['coordinates']
+            nuser = doc['user']
+            ntime = doc['created_at']
+            nplace = doc['place']
+            ndoc = {'_id': nid, 'text': ntext, 'user': nuser,
+                    'coordinates': ncoordinates, 'create_time': ntime,
+                    'place': nplace, 'addressed': False}
+            db.save(ndoc)
+            print(nid)
+            print('-------------------------------------')
+
     except UnicodeEncodeError:
         errorCount += 1
-        print "UnicodeEncodeError,errorCount ="+str(errorCount)
-
-print "completed, errorCount ="+str(errorCount)+" total tweets="+str(count)
+        print "UnicodeEncodeError,errorCount =" + str(errorCount)
--- TwitterHavester/TweetsStreamDownload.py
+++ TwitterHavester/TweetsStreamDownload.py
@@ -1,15 +1,17 @@
-#Import the necessary methods from tweepy library
+# import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from couchdb import Server
-import couchdb
 import json
 
-remote_server = Server()
-db = remote_server['stream_tweets']
+server = Server()
+try:
+    db = server['tweets']
+except:
+    db = server.create('tweets')
 
-#Variables that contains the user credentials to access Twitter API 
+# Variables that contains the user credentials to access Twitter API
 access_token = "546643485-WaT1mm4EwJe2RrnxMk5xfNxiUxnvfHc3HQgk6jFO"
 access_token_secret = "GbaChVqU98h8NpUQ1FzfSG2AonWFBVdtalv5d9LNDfUrU"
 consumer_key = "Orgmlwvc3OVi8UtyB1Idk1ArM"
@@ -18,33 +20,31 @@ consumer_secret = "GItv17P5pNOqtf2eRnjpfTTyuveo4LoCHUw4OZz3HJtEOo7i7p"
 
 class ReaderListener(StreamListener):
 
-
     def on_data(self, data):
         try:
-           # with open(self.outfile, 'a') as f:
-            #    f.write(data)
-             #   print(data)
-                
-           
+
             doc = json.loads(data)
-       
             nid = doc["id_str"]
-     
-            ntext = doc['text']
-  
-            ncoordinates = doc['coordinates']
-            nuser = doc['user']
-            ntime = doc['created_at']
-            nplace = doc['place']
-           # nid = content._json.u'id'
-            ndoc={'_id':nid,'text':ntext, 'user':nuser, 'coordinates':ncoordinates, 'create_time':ntime,'place':nplace,'addressed':False}
-            db.save(ndoc)
-            print(nid)
-            print('-------------------------------------')
-            return True
+
+            if nid in db:
+                print('--------aleardy have----------------')
+                return True
+            else:
+                ntext = doc['text']
+                ncoordinates = doc['coordinates']
+                nuser = doc['user']
+                ntime = doc['created_at']
+                nplace = doc['place']
+                ndoc = {'_id': nid, 'text': ntext, 'user': nuser,
+                        'coordinates': ncoordinates, 'create_time': ntime,
+                        'place': nplace, 'addressed': False}
+                db.save(ndoc)
+                print(nid)
+                print('-------------------------------------')
+                return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
-        
+
         return True
 
     def on_error(self, status):
@@ -54,11 +54,14 @@ class ReaderListener(StreamListener):
 
 if __name__ == '__main__':
 
-#This handles Twitter authetification and the connection to Twitter Streaming API
-    l = ReaderListener()
+    # This handles Twitter authetification and the connection to Twitter
+    # Streaming API
+    listener = ReaderListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+    stream = Stream(auth, listener)
 
-    #This line filter Twitter Streams to capture data by the keywords: '.', almost all tweets
-    stream.filter(track=["Trump","Donald Trump","President Trump"],languages=["en"])
+    # This line filter Twitter Streams to capture data by the keywords: '.',
+    # almost all tweets
+    stream.filter(track=["Trump", "Donald Trump",
+                         "President Trump"], languages=["en"])
